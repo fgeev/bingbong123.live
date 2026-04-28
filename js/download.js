feae7f7a857e1229b37ab73187e26e6d635a5b58
fgeev <128429461+fgeev@users.noreply.github.com>
2026-04-28 13:04:04 -0400

    Refactor download logic and add file download function
    Removed login checks and added downloadLevelFile function for downloading level files.

--- js/download.js
+++ js/download.js
@@ -1,21 +1,14 @@
-const urlParams = new URLSearchParams(window.location.search);
-const level = urlParams.get('level');
-if (level) {
-    let user_id = localStorage.getItem('user_id');
-    let user_name = localStorage.getItem('user_name');
-    let isLoggedIn = (user_id && user_name);
-    if (!isLoggedIn) {
-        document.getElementById('login').click();
-    } else {
-        if (!localStorage.getItem('isBlocked')) {
-            console.log(level);
-            const levels = level.split(" ");
-            alert("Use https://grabvr.tools for downloading");
-        } else {
-            const levels = level.split(" ");
-            for (let i = 0; i < levels.length; i++) {
-                log("BLOCKED", levels[i]);
-            }
-        }
-    }
+function downloadLevelFile(levelName, levelData) {
+    const blob = new Blob([levelData], { type: "application/octet-stream" });
+    const url = URL.createObjectURL(blob);
+
+    const a = document.createElement("a");
+    a.href = url;
+    a.download = levelName + ".level";
+
+    document.body.appendChild(a);
+    a.click();
+    a.remove();
+
+    URL.revokeObjectURL(url);
 }
